479e849d5e1856aa16339740ecc707d9d0145869
Robert Peralta <rapito@gmail.com>
2022-05-10 02:02:17 -0400

    use gcc/g++ 7 for linux

--- build-linux.sh
+++ build-linux.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -e
 # build-linux.sh
 
-CMAKE_FLAGS='-DLINUX_LOCAL_DEV=true'
+CMAKE_FLAGS='-DLINUX_LOCAL_DEV=true -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7'
 
 DATA_SYS_PATH="./Data/Sys/"
 BINARY_PATH="./build/Binaries/"
 
 # Move into the build directory, run CMake, and compile the project
-mkdir -p build
-pushd build
-cmake ${CMAKE_FLAGS} ../
+mkdir -p linux-build
+pushd linux-build
+cmake .. ${CMAKE_FLAGS}
 make -j$(nproc)
 popd
 
 # Copy the Sys folder in
 cp -r -n ${DATA_SYS_PATH} ${BINARY_PATH}
 
-touch ./build/Binaries/portable.txt
+touch ./linux-build/Binaries/portable.txt
